182e4e91222a94d744094b1a4bdad026ef7af22e
iknowueterent <semernya38@gmail.com>
2023-12-17 17:07:05 +0200

    complete task 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form");
 
+const sendFormInfo = (event) => {
+  event.preventDefault();
+
+  const elCollection = event.target.elements;
+
+  if (elCollection.email.value === "" || elCollection.password.value === "") {
+    alert("All form fields must be filled in!");
+  } else {
+    const formInfo = {
+      email: elCollection.email.value,
+      password: elCollection.password.value,
+    };
+    console.log(formInfo);
+  }
+
+  form.reset();
+};
+
+form.addEventListener("submit", sendFormInfo);
