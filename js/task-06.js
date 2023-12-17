e94d259fb1a6c715f09ce28829b5142c1ad71d09
iknowueterent <semernya38@gmail.com>
2023-12-17 20:12:53 +0200

    complete task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,5 +1,43 @@
+const input = document.querySelector("#controls input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxContainer = document.querySelector("#boxes");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  if (amount >= 1 && amount <= 100) {
+    const boxArr = [];
+    let size = 30;
+    for (let i = 0; i < amount; i++) {
+      size += 10;
+      const color = getRandomHexColor();
+      const box = `<div style=" height:${size}px; width:${size}px; background-color:${color}; "></div>`;
+      boxArr.push(box);
+    }
+    boxContainer.innerHTML = boxArr.join("");
+    console.log(boxContainer);
+  }
+}
+
+function destroyBoxes() {
+  boxContainer.innerHTML = "";
+}
+
+const createBoxesOnClick = () => {
+  let amount = 0;
+  amount = input.value;
+  createBoxes(amount);
+  input.value = "";
+};
+
+const destroyBoxesOnClick = () => {
+  destroyBoxes(); 
+};
+
+createBtn.addEventListener("click", createBoxesOnClick);
+destroyBtn.addEventListener("click", destroyBoxesOnClick);
