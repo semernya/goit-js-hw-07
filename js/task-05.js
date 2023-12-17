404502c95b5bb82c1bd70d84034e3e5ce9c3768b
iknowueterent <semernya38@gmail.com>
2023-12-17 17:35:48 +0200

    complete task 5

--- js/task-05.js
+++ js/task-05.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const btn = document.querySelector(".change-color");
+const colorOutput = document.querySelector(".color");
+
+const changeColor = (event) => {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorOutput.textContent = color;
+};
+btn.addEventListener("click", changeColor);
